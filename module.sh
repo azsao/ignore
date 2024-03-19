fa9d74da82c542b0c72e72158e5069006c9785c0
azsão <152450334+azsao@users.noreply.github.com>
2024-03-19 16:29:57 -0700

    Update module.sh

--- module.sh
+++ module.sh
@@ -1,45 +1,48 @@
 #!/bin/bash
 
-CONFIG_DIR = ~/.config/nvim
+CONFIG_DIR=~/.config/nvim
 NVARCH=$(find / -type d -name "exampledir")
 
-# making sure neovim exists
+# making sure neovim directory exists
 function neovim {
-if [ -d “$CONFIG_DIR” ]; then
-rm -r ~/.config/nvim
-mkdir ~/.config/nvim
-else
-mkdir ~/.config/nvim
-fi
+    if [ -d "$CONFIG_DIR" ]; then
+        rm -r ~/.config/nvim
+        mkdir ~/.config/nvim
+    else
+        mkdir ~/.config/nvim
+    fi
 } 
 
 # makes sure the repository exists
 function repo {
-if [ -d ”$NVARCH” ]; then
-echo “Repository detected”
-else
-git clone https://github.com/ [REPOSITORY NAME]  
-fi 
+    if [ -d "$NVARCH" ]; then
+        echo "Repository detected"
+    else
+        git clone https://github.com/[REPOSITORY_NAME] $NVARCH
+    fi 
 }
-
-
-
 
 # un-modular the file from the repository
 function modular {
-# these are DIRs
-mv ~/$NVARCH/lua/ ~/.config/nvim
-mv ~/$NVARCH/modules ~/.config/nvim/lua/nvarch/
-mv ~/$NVARCH/cosmetics ~/.config/nvim/lua/nvarch/modules
-mv ~/$NVARCH/utility/ ~/.config/nvim/lua/nvarch/
-mv ~/$NVARCH/lang/ ~/.config/nvim/lua/nvarch/utility/
-# these are files
-mv ~/$NVARCH/init.lua ~/.config/nvim/
-mv ~/$NVARCH/lazy.lua ~/.config/nvim/lua/nvarch/
+    # these are directories
+    mv "$NVARCH"/lua/ ~/.config/nvim
+    mv "$NVARCH"/modules ~/.config/nvim/lua/nvarch/
+    mv "$NVARCH"/cosmetics ~/.config/nvim/lua/nvarch/modules
+    mv "$NVARCH"/utility/ ~/.config/nvim/lua/nvarch/
+    mv "$NVARCH"/lang/ ~/.config/nvim/lua/nvarch/utility/
+    # these are files
+    mv "$NVARCH"/init.lua ~/.config/nvim/
+    mv "$NVARCH"/lazy.lua ~/.config/nvim/lua/nvarch/
 }
 
 function ending {
-echo “Add”
-echo “ASCI”
-echo “Art”
+    echo "Add"
+    echo "ASCI"
+    echo "Art"
 } 
+
+# Call functions
+neovim
+repo
+modular
+ending
